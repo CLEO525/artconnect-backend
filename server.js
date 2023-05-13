eaedd01cf318ddc00e83e5988254a80db9c953f7
Jiyeon Kim <cleo0718@gmail.com>
2023-05-14 02:51:54 +0900

    로그인 api 설정
    기본 DB 설정
    auth, login api 구현

--- server.js
+++ server.js
@@ -1,58 +1,46 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-
-const shortid = require("shortid");
+const express = require("express");
+const bodyParser = require("body-parser");
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
+
 const adapter = new FileSync("db.json");
 const db = low(adapter);
+const app = express();
 
-server.use(middlewares);
-server.use(jsonServer.bodyParser);
-server.use(router);
+app.use(bodyParser.json());
 
-server.use((req, res, next) => {
-  res.status = 404;
-  next(Error("not found"));
-});
+const shortid = require("shortid");
+const user = require("./api/user");
 
-server.use((err, req, res, next) => {
-  console.log(err);
-  res.status(res.statusCode || 500);
-  res.json({ error: err.message || "internal server error" });
-});
+// //초기 테이블 설정
+// db.defaults({
+//   user: [],
+//   post: [],
+//   bookmark: [],
+// }).write();
 
-//초기 테이블 설정
-db.defaults({
-  user: [],
-  exhibition: [],
-  bookmark: [],
-}).write();
+// //초기 테이블에 데이터 입력
+// db.get("user")
+//   .push({
+//     userNo: shortid.generate(),
+//     id: "test2",
+//     pwd: "test2",
+//     name: "test2",
+//   })
+//   .write();
 
-//초기 테이블에 데이터 입력
-db.get("user")
-  .push({
-    userNo: shortid.generate(),
-    id: "test",
-    pwd: "test",
-    name: "test",
-  })
-  .write();
-
-db.get("exhibition")
-  .push({
-    eNo: shortid.generate(),
-    cat: "free",
-    eName: "test",
-    sDate: "2023-01-01",
-    eDate: "2023-12-31",
-    thumbNail: "",
-    info: "test",
-    img: "",
-  })
-  .write();
+// db.get("post")
+//   .push({
+//     eNo: shortid.generate(),
+//     cat: "free",
+//     eName: "test",
+//     sDate: "2023-01-01",
+//     eDate: "2023-12-31",
+//     thumbNail: "",
+//     info: "test",
+//     img: "",
+//   })
+//   .write();
 
 //북마크의 경우 새로 생겼을때 object join을 통하여 데이터 생성
 // db.get("bookmark")
@@ -63,11 +51,21 @@ db.get("exhibition")
 //   })
 //   .write();
 
-//포트 4000번으로 고정, 백엔드 실행시 콘솔창에서 user와 exhibition테이블의 데이터를 볼수 있음
-server.listen(4000, () => {
-  console.log("JSON Server is running");
-  console.log(db.get("user").value());
-  console.log(db.get("exhibition").value());
-});
+//login api
+app.post("/login", user.login);
 
-module.exports = server;
+//logout api
+app.post("/logout", user.logout);
+
+// app.get("/protected", verifyJWT, (req, res) => {
+//   const id = req.id;
+// });
+// app.post("/login", async (req, res) => {
+//   console.log("body", req.body);
+//   const id = req.body.id;
+//   const pwd = req.body.pwd;
+
+//포트 4000번으로 고정, 백엔드 실행시 콘솔창에서 user와 exhibition테이블의 데이터를 볼수 있음
+app.listen(4000, () => {
+  console.log("Server is running");
+});
